4ad2c47f7305a84921a05ba7e7db1f5a9340fc12
Luis Garzon <luisgarzon90@yahoo.com>
2018-05-10 15:55:54 -0400

    Got initial state to show up on scoreboard as a number.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      init: 0
+    }
+  
+  }
+  initialState(){
+    return {
+      init: 0
+    }
+  }
   render() {
     return (
      <div>
-       <h1>Hello</h1>
+       <h1>Scoreboard</h1>
+       <div>
+         Luis
+       </div>
+       <div><button>+</button></div>
+       <div>{this.state.init}</div>
+       <div><button>-</button></div>
      </div>
     );
   }
