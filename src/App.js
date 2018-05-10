4a319b5c55234418bfd92e97433dcb46689dcc00
Luis Garzon <luisgarzon90@yahoo.com>
2018-05-10 16:08:32 -0400

    added the binds for sub and add functions to get counter working.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,23 @@ class App extends Component {
     this.state = {
       init: 0
     }
+    this.add = this.add.bind(this);
+    this.sub = this.sub.bind(this);
   }
   initialState(){
     return {
       init: 0
     }
+  }
+  add(){
+    this.setState({
+      init: this.state.init + 1
+    })
+  }
+  sub(){
+    this.setState({
+      init: this.state.init -1
+    })
   }
   render() {
     return (
@@ -19,9 +31,9 @@ class App extends Component {
        <div>
          Luis
        </div>
-       <div><button>+</button></div>
+       <div><button onClick = {this.add}>+</button></div>
        <div>{this.state.init}</div>
-       <div><button>-</button></div>
+       <div><button onClick = {this.sub}>-</button></div>
      </div>
     );
   }
